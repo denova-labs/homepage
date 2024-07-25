b5764b468a60b7ad1dc17965cd4f2624c168a05a
Allen Cao <allen.m.cao@gmail.com>
2024-07-25 10:57:39 -0700

    Add linguify, platefull, onecard projects to page.

--- app/(marketing)/projects/page.tsx
+++ app/(marketing)/projects/page.tsx
@@ -7,6 +7,10 @@ import { constructMetadata } from "@/lib/utils";
 import { ComparePlans } from "@/components/pricing/compare-plans";
 import { PricingCards } from "@/components/pricing/pricing-cards";
 import { PricingFaq } from "@/components/pricing/pricing-faq";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import linguifyThumbnail from '@/public/_static/projects/linguify.png'
+import platefullThumbnail from '@/public/_static/projects/platefull.png'
+import onecardThumbnail from '@/public/_static/projects/onecard.png'
 
 export const metadata = constructMetadata({
   title: "Pricing – SaaS Starter",
@@ -23,6 +27,23 @@ export default async function ProjectsPage() {
 
   return (
     <div className="flex w-full flex-col gap-16 py-8 md:py-8">
+      <HeroParallax products={[
+        {
+          title: "Linguify",
+          link: "https://chromewebstore.google.com/detail/linguify/geonaepbohggoefocbfgkifmlbafapfn",
+          thumbnail: linguifyThumbnail
+        },
+        {
+          title: "Platefull",
+          link: "https://platefull.denova.ai",
+          thumbnail: platefullThumbnail
+        },
+        {
+          title: "OneCard",
+          link: "https://onecard.denova.ai",
+          thumbnail: onecardThumbnail
+        },
+      ]}/>
       <PricingCards userId={user?.id} subscriptionPlan={subscriptionPlan} />
       <hr className="container" />
       <ComparePlans />
