d123fd7f3d81eb2e14b7ac115161ec5e1e133213
Allen Cao <allen.m.cao@gmail.com>
2024-07-27 08:45:22 -0700

    Update hero animation to shake.

--- components/ui/iconsdish.tsx
+++ components/ui/iconsdish.tsx
@@ -13,12 +13,40 @@ const icons = [
   <Icons.sun />,
   <Icons.hearthandshake />,
 ]
+const getRandomTransformOrigin = () => {
+  const value = (16 + 40 * Math.random()) / 100;
+  const value2 = (15 + 36 * Math.random()) / 100;
+  return {
+    originX: value,
+    originY: value2
+  };
+};
+
+const getRandomDelay = () => -(Math.random() * 0.7 + 0.05);
+
+const randomDuration = () => Math.random() * 0.07 + 0.23;
+
+const variants = {
+  initial: {rotate: 0, opacity: 0.5},
+  animation: (i) => ({
+    rotate: i % 2 === 0 ? [-1, 1.3, 0] : [1, -1.4, 0],
+    transition: {
+      delay: getRandomDelay(),
+      repeat: Infinity,
+      duration: randomDuration()
+    }
+  }),
+  reset: {
+    rotate: 0
+  }
+};
 
 export const IconsDish = ({
     className,
   }: {
     className?: string;
   }) => {
+
     return (
       <motion.div
         
@@ -26,10 +54,16 @@ export const IconsDish = ({
       >
         {icons.map((icon,index) => (
           <motion.span 
-            initial={{ opacity: 0.5, y: 110 }}
-            whileInView={{ opacity: 0.85, y: 100, color: "#c4b5fd" }}
+            style={{
+              ...getRandomTransformOrigin(),
+            }}
+            // -- controls --
+            // custom={index}
+            // variants={variants}
+            initial={{ rotate: 0, opacity: 0.1, y: 110 }}
+            animate={{ rotate: [0,5,-5,10, -10,10,-20,20,-10, 10, 0], opacity: 0.9, y: 100, color: "#c4b5fd" }}
             transition={{
-              delay: Math.floor(Math.random()*3)*.4 + 0.3,
+              delay: Math.floor(Math.random()*4)*.4 + 0.5,
               duration: 1.1,
               ease: "easeInOut",
             }}
